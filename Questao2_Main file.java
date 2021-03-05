ec98aaa3b2becee8fb7c69b018882102c6e7cab3
Vitor Barbosa Fonteles <73004721+Vito-Barbossa-Fonteles@users.noreply.github.com>
2021-03-05 18:07:04 -0300

    Create Questao2_Main file.java

---
+++ Questao2_Main file.java
@@ -0,0 +1,14 @@
+package Main;
+
+import Objetos.Pera;
+
+public class Exercicio_2_Pera {
+	public static void main(String args[]) {
+		Pera pera= new Pera();
+		pera.Calcular_valor_pera();
+		
+		
+		
+	}
+
+}
